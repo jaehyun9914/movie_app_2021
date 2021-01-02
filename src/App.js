4df65d63cd0f5a4c55da536e810a48e36a0b184f
jaehyun9914 <jaehyun9914@gmail.com>
2021-01-02 17:36:35 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({name, picture}){
+  return (
+  <div>
+    <h2>I like {name}</h2>
+    <img src = {picture}/>
+  </div>
+  );
+}
+
+const foodLike = [
+  {
+    name: 'Kimchi',
+    image: 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png'
+  },
+  {
+    name: 'Pasta',
+    image: 'https://t1.daumcdn.net/liveboard/dailylife/8defeeef58a04d3888ddfe03b5722000.jpg'
+  },
+  {
+    name: 'Jamon',
+    image: 'https://img-cf.kurly.com/shop/data/goodsview/20180827/gv10000030667_1.jpg'
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    {foodLike.map(dish => (
+      <Food name={dish.name} picture={dish.image}/>
+      ))}
+  </div>
   );
 }
 
