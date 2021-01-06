c3caf4bfd70b402992f8d8dfedb14049dd4ca355
jaehyun9914 <jaehyun9914@gmail.com>
2021-01-06 18:31:12 +0900

    main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component{
   state = {
@@ -11,15 +13,37 @@ class App extends React.Component{
       data: {
         data: {movies},
      },
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating&limit=30');
     this.setState({movies, isLoading:false});
   };
   componentDidMount() {
     this.getMovies();
 }
   render(){
-    const {isLoading} = this.state;
-    return <div>{isLoading ? 'Loading...' : 'We are ready!'} </div>;
+    const {isLoading, movies} = this.state;
+    return (
+    <section class ='cotnainer'>
+      {isLoading ? (
+        <div class='loader'>
+          <span class='loader_text'> 'Loading...'</span>
+        </div>
+        ) : (
+        <div class = 'movies'>
+            {movies.map((movie)=>{
+        return  (
+        <Movie
+        key = {movie.id}
+        id = {movie.id}
+        year = {movie.year}
+        title = {movie.title}
+        summary = {movie.summary}
+        poster = {movie.medium_cover_image}
+        />
+      )})}
+        </div>
+      )}
+    </section>
+    );
   }
 }
 
